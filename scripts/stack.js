65948f346fc5d34eeb5bd01ade43e43e7accd05e
Isabella Chen <miss.izzyc@gmail.com>
2018-09-05 19:31:21 +0200

    add stack

---
+++ scripts/stack.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Stack
+
+function Stack() {
+  this.storage = {};
+  this.length = 0;
+}
+
+Stack.prototype.push = function (value) {
+  this.storage[this.length++] = value;
+  return true;
+};
+
+Stack.prototype.pop = function () {
+  const el = this.storage[--this.length];
+  delete this.storage[this.length];
+  const value = el ? el : null;
+  return value;
+};
+
+Stack.prototype.size = function () {
+  return this.length;
+};
+
